7b38131327c2708758b41b43361d5f701e3188d6
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-11-21 12:07:36 +0300

    first commit

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SigninForm = () => {
+  return (
+    <div>
+      <h2 className="h3-bold md:h2-blod pt-5 sm:pt-12"> Create a new account</h2>
+    </div>
+  )
+}
+
+export default SigninForm

beb037d31fb83431953786563e05bb0255932a52
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-11-27 19:01:08 +0300

    AuthentificationContext

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -8,17 +8,25 @@ import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage,} from "@/comp
 import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader";
-import { Link } from "react-router-dom";
-import { createUserAccount } from "@/lib/appwrite/api";
+import { Link ,useNavigate } from "react-router-dom";
 import { useToast } from "@/components/ui/use-toast"
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 
 
 const SignupForm = () => {
 
 
-  const isLoading = false;
   const {toast} = useToast();
+
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+
+  const { mutateAsync: createUserAccount, isLoading: isCreatingAccount} = useCreateUserAccount();
+
+  const { mutateAsync: signInAccount, isLoading: isSignInAccount } = useSignInAccount();
+
+  const navigate = useNavigate();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -40,6 +48,26 @@ const SignupForm = () => {
       return toast({
         title: "Sign up failed. Please try again."
       })
+    }
+
+    const session = await signInAccount({
+      email:values.email,
+      password:values.password
+    })
+
+    if(!session){
+      return toast({
+        title: "Sign in failed, please try again."
+      })
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if(isLoggedIn){
+      form.reset();
+      navigate('/')
+    }else {
+      return toast({title: 'Sign up failed. Please try again.'})
     }
   }
 
@@ -107,7 +135,7 @@ const SignupForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isLoading ? (
+            {isCreatingAccount ? (
               <div className="flex-center gap-2">
                 <Loader/> Loading...
               </div>
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-import { ID } from  'appwrite';
+import { ID, Query } from  'appwrite';
 
 import { INewUser } from "@/types";
 import { account, avatars, databases, appwriteConfig } from './config';
@@ -48,6 +48,36 @@ export async function saveUserToDB(user: {
 
         return newUser;
     } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function signInAccount(user: {email: string; password: string}){
+    try {
+        const session = await account.createEmailSession(user.email, user.password);
+
+        return session;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getCurrentUser(){
+    try{
+        const currentAccount = await account.get();
+
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal('accountId',currentAccount.$id)]
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    } catch(error){
         console.log(error);
     }
 }

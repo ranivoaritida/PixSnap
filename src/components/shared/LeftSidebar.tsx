adfc0a5ba6210852b057646a5626ef4e0643a31f
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-12-01 12:14:06 +0300

    bottombar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -51,13 +51,13 @@ const LeftSidebar = () => {
             return (
               <li key={link.label} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`} >
                 <NavLink to={link.route} className="flex gap-4 items-center p-4" >
+                  <img 
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+                  />
                   {link.label}
                 </NavLink>
-                <img 
-                  src={link.imgURL}
-                  alt={link.label}
-                  className={`group-hover:invert-white ${isActive && 'invert-white'}`}
-                />
               </li>
             )
           })}

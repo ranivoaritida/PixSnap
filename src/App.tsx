103dd0fe13347d846e22f6dbaae7b52ce8ab7c9e
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-12-04 15:36:47 +0300

    create post

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
                 <Route path="/explore" element={<Explore />} />
                 <Route path="/saved" element={<Saved />} />
                 <Route path="/all-users" element={<AllUsers/>} />
-                <Route path="created-post" element={<CreatePost />} />
+                <Route path="create-post" element={<CreatePost />} />
                 <Route path="/update-post/:id" element={<EditPost />} />
                 <Route path="/posts/:id" element={<PostDetails />} />
                 <Route path="/profile:/id/*" element={<Profile />} />

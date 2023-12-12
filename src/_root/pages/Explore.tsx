263d1dc47316e6b1d53965d87a78334b0862cba9
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-12-12 17:53:05 +0300

    Explore page header

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -3,12 +3,17 @@ import React, { useState } from 'react'
 
 const Explore = () => {
 
+  const posts = [];
   const [searchValue, setSearchValue] = useState('');
 
+  const shouldShowSearchResults = searchValue !== '';
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every
+  ((item) => item.documents.length === 0)
+
   return (
-    <div className='explore_container'>
+    <div className='explore-container'>
       <div className='explore-inner_container'>
-        <h2 className='h3-bold md:h2-bold w-full'>Search posts</h2>
+        <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
         <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
           <img 
             src='/assets/icons/search.svg'
@@ -26,6 +31,27 @@ const Explore = () => {
         </div>
       </div>
       
+      <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
+      <h3 className='body-bold md:h3-bold'>Popular Today</h3>
+      <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
+        <p className='small-medium md:base-medium text-light-2'>All</p>
+        <img
+          src="/assets/icons/filter.svg"
+          width={20}
+          height={20}
+          alt='filter'
+        />
+      </div>
+      </div>
+      <div className='flex flex-wrap gap-9 w-full max-w-5xl'>
+        {shouldShowSearchResults ? (
+          <SearchResults />
+        ) : shouldShowPosts ? (
+          <p className='text-light-4 mt-10 text-center w-full'>End of posts</p>
+        ) : posts.pages.map((item,index) => (
+          <GridPostList key={`pages-${index}`} posts={item.documents} />
+        )) }
+      </div>
     </div>
   )
 }
